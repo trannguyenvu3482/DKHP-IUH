3973a28c6e1d9136eee8a7fb2d912c49c239e0f0
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2022-06-13 22:29:29 +0700

    finish xemChiTietLop function

--- popup.js
+++ popup.js
@@ -4,10 +4,15 @@ const $$ = document.querySelectorAll.bind(document);
 // variables
 const navTabs = $('.navigation-tabs');
 const getClassListBtn = $('.laydslhp-submit-btn');
+const getClassInfoBtn = $('.xemChiTiet-submit-btn');
+const getClassClearTableBtn = $('.laydslhp-clear-btn');
+const xemChiTietClearTableBtn = $('.xemChiTiet-clear-btn');
 const submitLHPBtn = $('.dkn-submit-btn');
 const resultContent = $('.section__result__table');
+const xemChitietTable = $('.section__result__xemChiTiet__table');
 const resultTextBox = $('.section__result__loop');
 const sectionDKNResultTextBox = $('.section-DKN__result__box');
+const xemChiTietCurrentId = $('.section__result__idHienTai');
 const resultClasses = [];
 
 // Tab switching handler
@@ -28,6 +33,7 @@ navTabs.addEventListener('click', (e) => {
 
 // Register button handler
 document.addEventListener('DOMContentLoaded', () => {
+  // Check if the url is correct and user is login
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     let url = tabs[0].url.toLowerCase();
 
@@ -132,7 +138,116 @@ document.addEventListener('DOMContentLoaded', () => {
     mainFunc();
   });
 
+  // Handle delete getClassList table btn
+  getClassClearTableBtn.addEventListener('click', () => {
+    resultContent.innerHTML = `
+      <tr style="background-color: #50a9f2">
+        <th style="padding-left: 10px">ID</th>
+        <th>Lớp dự kiến</th>
+        <th style="padding-right: 10px">Tình trạng hiện tại</th>
+      </tr>
+    `;
+  });
+
   // Xem chi tiet LHP
+  getClassInfoBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    let inputObject = {
+      idLopHocPhan: $('.section-xemChiTiet__idLopHoc__input').value,
+    };
+
+    console.log(inputObject);
+
+    const fetchData = async () => {
+      try {
+        let myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+
+        let urlencoded = new URLSearchParams();
+        urlencoded.append('idLopHocPhan', inputObject.idLopHocPhan.trim());
+        urlencoded.append('maNhomTH', -1);
+
+        let requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: urlencoded,
+          redirect: 'follow',
+        };
+
+        const url =
+          'https://dkhp.iuh.edu.vn/DangKyHocPhan/GetChiTietLopHocPhan';
+
+        const response = await fetch(url, requestOptions);
+        const data = await response.json();
+        if (data) {
+          localStorage.setItem('result', JSON.stringify(data));
+        }
+      } catch (error) {
+        $('.section__result__xemChiTiet__loop').textContent =
+          'Lỗi khi lấy dữ liệu, vui lòng kiểm tra các trường đã nhập!';
+      }
+    };
+
+    const renderData = async () => {
+      console.log('RENDERING!');
+      // Get result from localstorage and turn into JSON
+      const result = localStorage.getItem('result');
+      const resData = JSON.parse(result);
+      const resultData = resData.ListLichHoc;
+
+      console.log(resultData);
+
+      // Nếu không có ID thì thoát hàm
+      if (!resultData) return;
+
+      // Set ID hiện tại
+      xemChiTietCurrentId.textContent = $(
+        '.section-xemChiTiet__idLopHoc__input'
+      ).value;
+
+      // Render result
+      let resultHTML = ``;
+
+      // Render each item to table
+      resultData.forEach((item) => {
+        console.log('RENDER DATA!');
+        let resultJSONHtml = `
+        <tr>
+          <td style="padding-left: 10px;">${item.LichHoc}</td>
+          <td>${item.Phong}</td>
+          <td>${item.GiangVien}</td>
+          <td style="padding-right: 10px;">${item.NhomThucHanh || ''}</td>
+          </tr>`;
+        if (resultJSONHtml !== undefined) {
+          resultHTML += resultJSONHtml;
+        }
+      });
+
+      console.log(resultHTML);
+      xemChitietTable.insertAdjacentHTML('beforeend', resultHTML);
+    };
+
+    // Main function, use async-await to wait for fetching to be done
+    const mainFunc = async () => {
+      await fetchData();
+      await renderData();
+    };
+
+    mainFunc();
+  });
+
+  // Handle delete xemChiTiet table btn
+  xemChiTietClearTableBtn.addEventListener('click', () => {
+    xemChitietTable.innerHTML = `
+    <tr style="background-color: #50a9f2">
+      <th style="padding-left: 10px">Lịch học</th>
+      <th>Phòng</th>
+      <th>Giảng viên</th>
+      <th style="padding-right: 10px">Nhóm TH</th>
+    </tr>
+    `;
+  });
 
   // Dang ki hoc phan
   submitLHPBtn.addEventListener('click', async (e) => {
